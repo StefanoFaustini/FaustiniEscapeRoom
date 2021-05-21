4721bf00af2906a30e317836ce52d20cd4c5e3f2
work <work@DESKTOP-KHE2GLA>
2021-05-21 11:04:37 +0200

    Aggiunta funzione elimina accesso

--- src/main/java/com/mycompany/fineanno/EscapeRoom.java
+++ src/main/java/com/mycompany/fineanno/EscapeRoom.java
@@ -12,13 +12,16 @@ package com.mycompany.fineanno;
 public class EscapeRoom {
     
     private Accesso[] elencoAccessi;
+    private Accesso[] elencoStanze;
     private final int N_MAX_ACCESSI=3;
     private int accessiPresenti;
-
+    private int stanzeOccupate;
     public EscapeRoom() 
     {
-        elencoAccessi= new Accesso[N_MAX_ACCESSI];
+        elencoAccessi= new Accesso[100];
+        elencoStanze= new Accesso[N_MAX_ACCESSI];
         this.accessiPresenti=0;
+        this.stanzeOccupate=0;
     }
     
     
@@ -41,12 +44,12 @@ public class EscapeRoom {
     
     public int aggiungiAccesso(Accesso a1)
     {
-        for (int i=0; i<elencoAccessi.length; i++)
+        for (int i=0; i<elencoStanze.length; i++)
         {
-            if(elencoAccessi[i]==null)
+            if(elencoStanze[i]==null)
             {
-                elencoAccessi[i]=new Accesso(a1);
-                this.accessiPresenti++;
+                elencoStanze[i]=new Accesso(a1);
+                this.stanzeOccupate++;
                 return 0;
             }
            
@@ -55,6 +58,23 @@ public class EscapeRoom {
     }
     
     
+    public int eliminaAccesso(int codiceFiscale)
+    {
+         for (int i=0; i<elencoStanze.length; i++)
+        {
+            if(elencoStanze[i]!=null)
+            {
+                if(elencoStanze[i].getCodiceFiscale()==codiceFiscale)
+                {
+                    elencoAccessi[accessiPresenti]=new Accesso(elencoStanze[i]);
+                    elencoStanze[i]=null;
+                    stanzeOccupate--;
+                    return 0;
+                }
+            } 
+        }
+        return -1;
+    }
     
-    
+       
 }
